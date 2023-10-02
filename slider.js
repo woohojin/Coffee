be9e38e2dae94004b757e21c0ed41c730fd92028
woohojin <83582360+woohojin@users.noreply.github.com>
2023-10-02 15:20:55 +0900

    auto slide

--- slider.js
+++ slider.js
@@ -9,6 +9,39 @@ window.onload = function () {
   //맨 마지막 이미지부터 보이기 때문에 currentIdx가 next는 - prev는 +로 진행됨
 
   let currentIdx = sliderList.length - 1;
+
+  slideInterval = setInterval(function () {
+    if (currentIdx != 0) {
+      sliderList[currentIdx - 1].classList.add("fade_in");
+      sliderList[currentIdx - 1].classList.remove("fade_out");
+      sliderList[currentIdx].classList.add("fade_out");
+      sliderList[currentIdx].classList.remove("fade_in");
+      currentIdx -= 1;
+    } else if (currentIdx <= 0) {
+      for (var i = currentIdx; i <= sliderList.length - 1; i++) {
+        sliderList[i].classList.add("fade_out");
+        if (i === 0) {
+          sliderList[i].classList.remove("fade_in");
+        } else if (i === sliderList.length - 1) {
+          sliderList[i].classList.remove("fade_out");
+          sliderList[i].classList.add("fade_in");
+          currentIdx = sliderList.length - 1;
+        }
+      }
+    } else if (currentIdx >= sliderList.length - 1) {
+      for (var i = currentIdx; i >= 0; i--) {
+        sliderList[i].classList.add("fade_out");
+        if (i === 0) {
+          sliderList[i].classList.add("fade_in");
+          sliderList[i].classList.remove("fade_out");
+        } else if (i === sliderList.length - 1) {
+          sliderList[i].classList.remove("fade_in");
+          currentIdx = 0;
+        }
+      }
+    }
+  }, 6000);
+
   moveButton.addEventListener("click", moveSlide);
   function moveSlide(event) {
     event.preventDefault();
@@ -31,7 +64,6 @@ window.onload = function () {
         sliderList[currentIdx].classList.remove("fade_in");
         currentIdx -= 1;
       }
-      console.log(currentIdx);
     } else if (event.target.className === "prev") {
       if (currentIdx >= sliderList.length - 1) {
         for (var i = currentIdx; i >= 0; i--) {
