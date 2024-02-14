5037c8c69ce6d7c738c3716d54a2087e7e91615c
woohojin <hojin0624@gmail.com>
2024-02-14 14:53:58 +0900

    delete target files

--- target/da_Coffee/view/js/searchSubmit.js
+++
@@ -1,7 +0,0 @@
-function searchSubmit() {
-  if(document.getElementById("search_text").value === "") {
-    alert("검색어를 입력해주세요.");
-    return false;
-  }
-  document.getElementById("search_form").submit();
-}
