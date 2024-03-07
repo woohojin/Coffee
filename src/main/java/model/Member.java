b17508490a3a3e0c57bf834b0bd62f70df8d83f3
woohojin <hojin0624@gmail.com>
2024-03-07 13:27:21 +0900

    update memberMapper

--- src/main/java/model/Member.java
+++ src/main/java/model/Member.java
@@ -20,6 +20,7 @@ public class Member {
     String memberDisableDate;
     String memberModifierName;
     String memberModifiedDate;
+    String memberWithdrawalDate; // 모델에서만 필요함
 
     public String getMemberId() {
         return memberId;
@@ -173,29 +174,12 @@ public class Member {
         this.memberModifiedDate = memberModifiedDate;
     }
 
-    @Override
-    public String toString() {
-        return "Member{" +
-                "memberId='" + memberId + '\'' +
-                ", memberName='" + memberName + '\'' +
-                ", memberCompanyName='" + memberCompanyName + '\'' +
-                ", memberPassword='" + memberPassword + '\'' +
-                ", memberTel='" + memberTel + '\'' +
-                ", memberCompanyTel='" + memberCompanyTel + '\'' +
-                ", memberAddress='" + memberAddress + '\'' +
-                ", memberDetailAddress='" + memberDetailAddress + '\'' +
-                ", memberDeliveryAddress='" + memberDeliveryAddress + '\'' +
-                ", memberDetailDeliveryAddress='" + memberDetailDeliveryAddress + '\'' +
-                ", memberEmail='" + memberEmail + '\'' +
-                ", memberFile='" + memberFile + '\'' +
-                ", memberFranCode='" + memberFranCode + '\'' +
-                ", memberTier=" + memberTier +
-                ", memberDate='" + memberDate + '\'' +
-                ", memberDisable=" + memberDisable +
-                ", memberDisableDate='" + memberDisableDate + '\'' +
-                ", memberModifierName='" + memberModifierName + '\'' +
-                ", memberModifiedDate='" + memberModifiedDate + '\'' +
-                '}';
+    public String getMemberWithdrawalDate() {
+        return memberWithdrawalDate;
+    }
+
+    public void setMemberWithdrawalDate(String memberWithdrawalDate) {
+        this.memberWithdrawalDate = memberWithdrawalDate;
     }
 }
 
