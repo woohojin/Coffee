5a3bc135665f796c4e96ce7392a3397b4ca2d83e
Woohojin <hojin0624@gmail.com>
2024-03-08 20:09:26 +0900

    Revert "delete not used file paymentsRequest.java"
    This reverts commit dc03f93a676d1eed7a87915f3c1c7e2019d41adf.

---
+++ src/main/java/model/PaymentsRequest.java
@@ -0,0 +1,31 @@
+package model;
+
+public class PaymentsRequest {
+    private String paymentKey;
+    private String orderId;
+    private String amount;
+
+    public String getPaymentKey() {
+        return paymentKey;
+    }
+
+    public void setPaymentKey(String paymentKey) {
+        this.paymentKey = paymentKey;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+}
