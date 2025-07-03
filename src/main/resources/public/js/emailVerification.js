5dae7fcc95c601f660d914b15de0ed141067fe90
woohojin <83582360+woohojin@users.noreply.github.com>
2025-07-03 13:34:33 +0900

    Refactor emailVerification xhr to fetch

--- src/main/resources/public/js/emailVerification.js
+++ src/main/resources/public/js/emailVerification.js
@@ -4,56 +4,99 @@ let isVerified = false;
 let isEmailSending = false;
 const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-function verifyEmail() {
+async function verifyEmail() {
   let memberEmail = document.querySelector('.member_email').value;
 
-  if(emailRegex.test(memberEmail) === false) {
+  if (emailRegex.test(memberEmail) === false) {
     alert("이메일 형식이 올바르지 않습니다.");
     return;
-    }
-
-  if(!isEmailSending && !isVerified) {
-    isEmailSending = true;
-
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', '../member/verifyEmail', true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-    let csrfToken = document.querySelector('meta[name="_csrf"]').getAttribute('content');
-    let csrfHeader = document.querySelector('meta[name="_csrf_header"]').getAttribute('content');
-    xhr.setRequestHeader(csrfHeader, csrfToken);
-
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-          let response = JSON.parse(xhr.responseText);
-          code = response.code;
-
-          if (countdownInterval) {
-            clearInterval(countdownInterval);
-          }
-
-          alert("인증번호가 전송되었습니다.");
-          startCountdown();
-
-          setTimeout(() => {
-            isEmailSending = false;
-          }, 60000);
-
-        } else {
-          console.error('요청 실패 :', xhr.status);
-        }
-      }
-    };
-    let params = 'memberEmail=' + encodeURIComponent(memberEmail);
-    xhr.send(params);
-  } else if(isVerified){
-    alert("이미 인증하셨습니다.");
-  } else {
-    alert("1분뒤에 인증번호를 재전송 할 수 있습니다.");
   }
 
+  if (!isEmailSending && !isVerified) {
+    isEmailSending = true;
+
+    const csrfToken = document.querySelector('meta[name="_csrf"]').getAttribute('content');
+    const csrfHeader = document.querySelector('meta[name="_csrf_header"]').getAttribute('content');
+
+    const params = new URLSearchParams();
+    params.append("memberEmail", memberEmail);
+
+    try {
+      const response = await fetch("../member/verifyEmail", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          [csrfHeader]: csrfToken
+        },
+        body: params
+      });
+
+      if (response.ok) {
+        const responseData = await response.json();
+        code = responseData.code;
+
+        if (countdownInterval) {
+          clearInterval(countdownInterval);
+        }
+
+        alert("인증번호가 전송되었습니다.");
+        startCountdown();
+        document.querySelector(".verify_code").focus();
+
+        setTimeout(() => {
+          isEmailSending = false;
+        }, 60000);
+      } else {
+        console.error("요청 실패 : ", response.status);
+        alert("인증번호 발송에 실패했습니다.");
+      }
+    } catch (err) {
+      console.error("에러 : ", err);
+      alert("인증번호 발송에 실패했습니다.");
+    }
+  } else if (isVerified) {
+    alert("이미 인증하셨습니다.");
+  } else {
+    alert("1분 뒤에 인증번호를 재전송 할 수 있습니다.");
+  }
 }
+
+  //   let xhr = new XMLHttpRequest();
+  //   xhr.open('POST', '../member/verifyEmail', true);
+  //   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  //
+  //
+  //   xhr.setRequestHeader(csrfHeader, csrfToken);
+  //
+  //   xhr.onreadystatechange = function() {
+  //     if (xhr.readyState === XMLHttpRequest.DONE) {
+  //       if (xhr.status === 200) {
+  //         let response = JSON.parse(xhr.responseText);
+  //         code = response.code;
+  //
+  //         if (countdownInterval) {
+  //           clearInterval(countdownInterval);
+  //         }
+  //
+  //         alert("인증번호가 전송되었습니다.");
+  //         startCountdown();
+  //
+  //         setTimeout(() => {
+  //           isEmailSending = false;
+  //         }, 60000);
+  //
+  //       } else {
+  //         console.error('요청 실패 :', xhr.status);
+  //       }
+  //     }
+  //   };
+  //   let params = 'memberEmail=' + encodeURIComponent(memberEmail);
+  //   xhr.send(params);
+  // } else if(isVerified){
+  //   alert("이미 인증하셨습니다.");
+  // } else {
+  //   alert("1분뒤에 인증번호를 재전송 할 수 있습니다.");
+  // }
 
 function checkVerify() {
   let verifyCode = document.querySelector(".verify_code").value;
