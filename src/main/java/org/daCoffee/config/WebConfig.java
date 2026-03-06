4e04787fdc53877eb695d1e6add35b5b2b2a5779
woohojin <83582360+woohojin@users.noreply.github.com>
2026-03-06 12:26:03 +0900

    Refactor: replace @Autowired with @RequiredArgsConstructor in WebConfig

--- src/main/java/org/daCoffee/config/WebConfig.java
+++ src/main/java/org/daCoffee/config/WebConfig.java
@@ -1,19 +1,15 @@
 package org.daCoffee.config;
 
+import lombok.RequiredArgsConstructor;
 import org.daCoffee.service.interceptor.MemberInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
   private final MemberInterceptor memberInterceptor;
-
-  @Autowired
-  public WebConfig(MemberInterceptor memberInterceptor) {
-    this.memberInterceptor = memberInterceptor;
-  }
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
